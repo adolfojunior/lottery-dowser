5ac733d6c49379de2f4eaba0419b77473ce5dc86
adolfojunior <adolfojunior@gmail.com>
2017-12-24 11:15:51 -0200

    limit generation

--- src/lottery-dowser.js
+++ src/lottery-dowser.js
@@ -9,6 +9,7 @@ class LotteryDowser {
   run(options) {
     this.executeStatistics(options)
     this.suggestNumbers(options)
+    this.generateCombinations(options)
   }
 
   executeStatistics({ verbose }) {
@@ -16,16 +17,16 @@ class LotteryDowser {
     const rowStatistics = this.data.getRowStatistics()
     const numberStatistics = this.data.getNumberStatistics()
     const rowOcurrencyStatistics = this.data.getRowOcurrencyStatistics()
-  
+
     log(chalk`{whiteBright # By Number:}`)
     log(chalk` - statistics: {cyanBright %j}`, numberStatistics.stats())
-  
+
     if (verbose === true) {
       numberStatistics.iterate((number, total) => {
         const relations = this.data.getNumberRelations().get(number)
         const sortedRelations = relations.keys()
         const relationToString = (n) => `${padStart(n, 2)}(${padStart(numberStatistics.get(n), 3)})`
-  
+
         log(` * Number(%s), total: %s, relations: %s <...> %s`,
           padStart(number, 2),
           padStart(total, 3),
@@ -34,11 +35,11 @@ class LotteryDowser {
         )
       })
     }
-  
+
     log(chalk`{whiteBright # By Row:}`)
     log(chalk` - statistics : {yellow %j}`, rowStatistics.stats())
     log(chalk` - ocurrencies: {yellow %j}`, rowOcurrencyStatistics.values())
-  
+
     if (verbose === true) {
       rowStatistics.iterate((row, total) => {
         log(chalk` * Row(%s), total: {blueBright %s}, ocurrencies: {greenBright %j}`,
@@ -50,44 +51,50 @@ class LotteryDowser {
     }
   }
 
-  suggestNumbers({ generate, size, limit }) {
-  
-    const numbersByRelation = this.data.getNumbersItercalatedByRelation()
-    const suggestedNumbers = numbersByRelation.splice(0, size)
-  
+  suggestNumbers({ size }) {
+
+    const suggestedNumbers = this.data.getNumbersItercalatedByRelation().splice(0, size)
+
     log(chalk`{whiteBright # Suggestions:}`)
     log(chalk` - numbers     : {cyanBright %j}`, suggestedNumbers)
     log(chalk` - occurrencies: {magentaBright %j}`, this.data.getOcurrencyCount(suggestedNumbers).values())
-  
-    if (generate === true) {
-      log(chalk`{whiteBright # Combinations:} limit(%s)`, limit)
-  
-      let generated = 0
+  }
 
-      this.data.combineNumbers(numbersByRelation, size, (numbers, count) => {  
-        // stop it
-        if (limit && generated >= limit) {
-          return false
-        }
+  generateCombinations({ generate, size, limit }) {
 
-        const ocurrencies = this.data.getOcurrencyCount(numbers)
-        if (
-          ocurrencies.get(3) <= 3 &&
-          ocurrencies.get(4) === 0 &&
-          ocurrencies.get(5) === 0 &&
-          ocurrencies.get(6) === 0
-        ) {
-          generated++
-          log(chalk`* Combination: %s, numbers: %j, ocurrency: %j`,
-            padStart(count, 9),
-            numbers,
-            ocurrencies.values()
-          )
-        } else if (count % 10000 === 0) {
-          log(` - working...`, count)
-        }
-      })
+    if (!generate) {
+      return
     }
+
+    const numbersToCombine = this.data.getNumbersItercalatedByRelation()
+    const generationState = { count: 0 }
+
+    log(chalk`{whiteBright # Combinations:} limit(%s)`, limit)
+
+    this.data.combineNumbers(numbersToCombine, size, (numbers, count) => {
+      // stop it
+      if (limit && generationState.count >= limit) {
+        return false
+      }
+
+      const ocurrencies = this.data.getOcurrencyCount(numbers)
+      if (
+        // TODO add a strategy per dataset
+        ocurrencies.get(3) <= 3 &&
+        ocurrencies.get(4) === 0 &&
+        ocurrencies.get(5) === 0 &&
+        ocurrencies.get(6) === 0
+      ) {
+        log(chalk`* Combination: %s, numbers: %j, ocurrency: %j`,
+          count,
+          numbers,
+          ocurrencies.values()
+        )
+        generationState.count += 1
+      } else if (count % 10000 === 0) {
+        log(` - working...`, count)
+      }
+    })
   }
 }
 
